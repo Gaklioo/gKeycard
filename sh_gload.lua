283ccb6e5d47bfbb7bf53e95a2a806341267d378
Gaklio <43461159+Gaklioo@users.noreply.github.com>
2025-04-15 20:23:03 -0400

    Add files via upload
    stupid autoloader not loading things in the correct order >:(

--- sh_gload.lua
+++ sh_gload.lua
@@ -1,5 +1,6 @@
 gAccess = {}
-gAccess.BaseDir = "gaccesscontrol/"
+gAccess.BaseDir = "gaccesscontrol/lua/"
+gAccess.EntityDir = "gaccesscontrol/entities/"
 
 function gAccess.Log(...)
     local time = string.format("[%s]", os.date("%H:%M:%S"))
@@ -8,29 +9,29 @@ function gAccess.Log(...)
     MsgC(Color(149, 36, 255), "[gAccess Loader] ", Color(255, 255, 255, 255), time .. " " .. table.concat(args, " ") .. "\n")
 end
 
-function gAccess.LoadShared(file)
-    gAccess.Log("Loading File Shared " .. file)
+function gAccess.LoadShared(f)
+    gAccess.Log("Loading File Shared " .. f)
     if SERVER then
-        AddCSLuaFile(file)
-        include(file)
+        AddCSLuaFile(f)
+        include(f)
     else
-        include(file)
+        include(f)
     end
 end
 
-function gAccess.LoadServer(file)
-    gAccess.Log("Loading File Server " .. file)
+function gAccess.LoadServer(f)
+    gAccess.Log("Loading File Server " .. f)
     if SERVER then
-        include(file)
+        include(f)
     end
 end
 
-function gAccess.LoadClient(file)
-    gAccess.Log("Loading File Client " .. file)
+function gAccess.LoadClient(f)
+    gAccess.Log("Loading File Client " .. f)
     if SERVER then
-        AddCSLuaFile(file)
+        AddCSLuaFile(f)
     else
-        include(file)
+        include(f)
     end
 end
 
@@ -39,13 +40,13 @@ function gAccess.LoadEverything(basePath)
 
     for k, v in pairs(files) do
         local path = basePath .. v
-
-        if string.find(path, "cl_") then
-            gAccess.LoadClient(path)            
+      
+        if string.find(path, "sh_") then
+            gAccess.LoadShared(path)
         elseif string.find(path, "sv_") then
             gAccess.LoadServer(path)
-        else
-            gAccess.LoadShared(path)
+        elseif string.find(path, "cl_") then
+            gAccess.LoadClient(path)      
         end
     end
 
@@ -55,3 +56,6 @@ function gAccess.LoadEverything(basePath)
 end
 
 gAccess.LoadEverything(gAccess.BaseDir)
+gAccess.LoadShared("gaccesscontrol/entities/gbaseaccess/sh_shared.lua")
+gAccess.LoadServer("gaccesscontrol/entities/gbaseaccess/sv_init.lua")
+gAccess.LoadClient("gaccesscontrol/entities/gbaseaccess/cl_init.lua")
